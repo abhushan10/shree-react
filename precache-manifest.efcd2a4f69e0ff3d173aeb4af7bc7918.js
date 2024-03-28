f09c0f32809881fd4b07131dbda3ceb801fcd64f
abhushan10 <abhushan.bhandari@gmail.com>
2024-03-28 10:27:16 +0000

    Updates

---
+++ precache-manifest.efcd2a4f69e0ff3d173aeb4af7bc7918.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94dac5e73c63a44775e72de74b8702d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2514bd1fab6ab3ded5c1",
+    "url": "/static/css/2.5b0a96a7.chunk.css"
+  },
+  {
+    "revision": "64f51b66880b71bf568c",
+    "url": "/static/css/main.0952d85a.chunk.css"
+  },
+  {
+    "revision": "2514bd1fab6ab3ded5c1",
+    "url": "/static/js/2.c5e6cc52.chunk.js"
+  },
+  {
+    "revision": "7bff561a6172acfb08e5",
+    "url": "/static/js/3.b8243406.chunk.js"
+  },
+  {
+    "revision": "64f51b66880b71bf568c",
+    "url": "/static/js/main.43a6e555.chunk.js"
+  },
+  {
+    "revision": "ea66432ffd364ece06d8",
+    "url": "/static/js/runtime~main.4ca6afeb.js"
+  },
+  {
+    "revision": "593ec79958b81b1da6d36a6d4fb85831",
+    "url": "/static/media/1.593ec799.png"
+  },
+  {
+    "revision": "f67d803229a7697032eb2747cd508693",
+    "url": "/static/media/2.f67d8032.png"
+  },
+  {
+    "revision": "559879b94b4f18749aa37992634eec2b",
+    "url": "/static/media/3.559879b9.png"
+  },
+  {
+    "revision": "b79f5ff90718d9301b538cff9982b482",
+    "url": "/static/media/4.b79f5ff9.png"
+  },
+  {
+    "revision": "8eb480fcedf1bdfd13ea4007834bbd86",
+    "url": "/static/media/5.8eb480fc.png"
+  },
+  {
+    "revision": "5a02d9f94efdefa56532704e1c6a2ac8",
+    "url": "/static/media/6.5a02d9f9.png"
+  },
+  {
+    "revision": "43b7605a2265b6de03f6b534e10ce7b7",
+    "url": "/static/media/7.43b7605a.png"
+  },
+  {
+    "revision": "c3a6dff7dd0201e3e8a4b5cba412cc3a",
+    "url": "/static/media/8.c3a6dff7.png"
+  },
+  {
+    "revision": "7d33a0b6b86a93281215b216c1c7a9a4",
+    "url": "/static/media/9.7d33a0b6.png"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "fe9b07a8b3a5ab11dcc46bb72825cfff",
+    "url": "/static/media/Evie.fe9b07a8.png"
+  },
+  {
+    "revision": "b6b6f7a810121b7a4aa781bebcf6f2b8",
+    "url": "/static/media/Shree_Brow_bar_Price_list.b6b6f7a8.png"
+  },
+  {
+    "revision": "f750170aaddf4e5f885a27d391a311d9",
+    "url": "/static/media/arrow1.f750170a.svg"
+  },
+  {
+    "revision": "9070490babe4d1aa7f077b104555b363",
+    "url": "/static/media/arrow2.9070490b.svg"
+  },
+  {
+    "revision": "fe2ecd71b2895bc12f2d4d5aaa7d764f",
+    "url": "/static/media/banner_bg.fe2ecd71.jpg"
+  },
+  {
+    "revision": "44f251aa3d38282ea3c258659324a8c2",
+    "url": "/static/media/beautiful_girl_home.44f251aa.png"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "42c4dbf649597b4d4ceaf4c9792de60a",
+    "url": "/static/media/color-sharp2.42c4dbf6.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "fc18d83325ea0d6bfab62c9b8586f42f",
+    "url": "/static/media/hannah.fc18d833.png"
+  },
+  {
+    "revision": "d5d9e92083e4445803ccc703330765db",
+    "url": "/static/media/katie.d5d9e920.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "b70596ec805737efdb88f1fe272a3506",
+    "url": "/static/media/logo_for_linkedin.b70596ec.png"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "e33fc36995e1c4bce1ef527da791308f",
+    "url": "/static/media/shree_map.e33fc369.png"
+  }
+]);
